c5e9fee012df857483fa38508ab7cfe3de98022f
Ahmed Ali <a7med3li0111@gmail.com>
2025-02-03 17:47:31 +0200

    env commit

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connection() {
   await mongoose
-    .connect(process.env.DATABASEOFFLINE)
+    .connect(process.env.DATABASEONLINE)
     .then((res) => console.log("connection done"))
     .catch((err) => console.log("connection err"));
 }
