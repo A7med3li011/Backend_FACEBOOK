5c84826a060bfa499bee3396082c35e8dd578640
Ahmed Ali <a7med3li0111@gmail.com>
2025-02-06 02:31:58 +0200

    limit size for request

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ import postRouter from "./src/routes/post.routes.js";
 connection();
 const app = express();
 app.use(cors());
-app.use(express.json());
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/meta/facebook/user", userRoutes);
 app.use("/api/meta/facebook/user/post", postRouter);
